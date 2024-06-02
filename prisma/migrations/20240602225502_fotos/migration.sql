bc6c4fef0bbe8d085725d34c1abba1acad6a3f8c
Digo <digo.rodrigomalaquias@hotmail.com>
2024-06-02 19:55:23 -0300

    subindo fotos

---
+++ prisma/migrations/20240602225502_fotos/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Comodo" ADD COLUMN     "fotos" TEXT;
+
+-- AlterTable
+ALTER TABLE "Imovel" ADD COLUMN     "fotos" TEXT;
+
+-- AlterTable
+ALTER TABLE "Pessoa" ADD COLUMN     "fotos" TEXT;
