b14799b0e50d4d4d765fd84cdb1b5f51136f46b9
Digo <digo.rodrigomalaquias@hotmail.com>
2024-06-07 21:04:46 -0300

    subindo fotos

--- src/controller/ComponenteController.ts
+++ src/controller/ComponenteController.ts
@@ -11,9 +11,27 @@ const path = require("path");
 
 const credentials = require("./credentials.json");
 
-// Autentique a aplicação usando as credenciais
+// // AUTORIZAÇÃO PARA AMBIENTE TESTE LOCALHGOST
+// const auth = new google.auth.GoogleAuth({
+//   credentials,
+//   scopes: "https://www.googleapis.com/auth/drive",
+// });
+
+//PARA PRODUÇÃO
+
 const auth = new google.auth.GoogleAuth({
-  credentials,
+  credentials: {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  },
   scopes: "https://www.googleapis.com/auth/drive",
 });
 
